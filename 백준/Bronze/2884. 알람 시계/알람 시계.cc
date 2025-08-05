19899b450ac61d195b75a7ccb46230f14b4f81af
KSS095 <tmdtn339@gmail.com>
2025-08-06 01:14:55 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int H, M;
+	cin >> H >> M;
+
+	if (M < 45) {
+		if (H == 0) H = 23;
+		else H--;
+	}
+	M = ((M + 60) - 45) % 60;
+	cout << H << " " << M;
+}
