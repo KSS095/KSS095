fad411b53a4644e6cd54fc292dbbab20aee3cfc3
KSS095 <tmdtn339@gmail.com>
2025-08-06 01:04:47 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	string str;
+	while (true) {
+		getline(cin, str);
+
+		if (str == "#") break;
+
+		int cnt = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'
+				|| str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+				cnt++;
+		}
+		cout << cnt << "\n";
+	}
+}
