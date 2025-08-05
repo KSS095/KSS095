c4ef645f747af175351d168774ff887a23221e63
KSS095 <tmdtn339@gmail.com>
2025-08-06 01:29:15 +0900

    [Silver I] Title: 1로 만들기 2, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1000001];
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) arr[i] = i;
+
+	for (int i = 2; i <= N; i++) {
+		if (i % 2 == 0) arr[i] = min(arr[i], arr[i / 2]);
+		if (i % 3 == 0) arr[i] = min(arr[i], arr[i / 3]);
+		arr[i] = min(arr[i], arr[i - 1]) + 1;
+	}
+
+	cout << arr[N] - 1 << "\n";
+
+	while (N != 0) {
+		cout << N << " ";
+
+		if (N > 0) {
+			if (arr[N] == arr[N - 1] + 1) N -= 1;
+			else if (N % 2 == 0 && arr[N] == arr[N / 2] + 1) N /= 2;
+			else if (N % 3 == 0 && arr[N] == arr[N / 3] + 1) N /= 3;
+		}
+	}
+}
