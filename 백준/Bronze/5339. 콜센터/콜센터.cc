eebc94ce5bf5cb3b3f00db7b2fab457d9fdbd29a
KSS095 <tmdtn339@gmail.com>
2025-08-06 01:18:19 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "     /~\\" << "\n";
+	cout << "    ( oo|" << "\n";
+	cout << "    _\\=/_" << "\n";
+	cout << "   /  _  \\" << "\n";
+	cout << "  //|/.\\|\\\\" << "\n";
+	cout << " ||  \\ /  ||" << "\n";
+	cout << "============" << "\n";
+	cout << "|          |" << "\n";
+	cout << "|          |" << "\n";
+	cout << "|          |" << "\n";
+}
